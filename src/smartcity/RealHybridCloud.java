0a6ea7f63cec5a74d79365b2db6fe06ef2cb5113
Mahrad Hosseini <mahrad.hosseini@yahoo.com>
2024-12-24 21:18:15 +0200

    Update RealHybridCloud.java

--- src/smartcity/RealHybridCloud.java
+++ src/smartcity/RealHybridCloud.java
@@ -15,6 +15,13 @@ public class RealHybridCloud implements HybridCloud {
         System.out.println("Hybrid Cloud loaded from DB: " + data);
     }
 
+    /*
+    Is supposed to be a placeholder for Distributed Computing power
+
+    Takes in desired PredictionCase (either Traffic or Energy),
+    Chooses a random output from a list of predefined values.
+    Returns the predicted value plus the prediction case
+     */
     public String distributedComputation(String predictionCase){
         System.out.println("Hybrid Cloud computed: " + predictionCase);
         List<String> Vals;
